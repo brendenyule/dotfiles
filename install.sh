5803118085af81261e155b6383793b04ce058e0e
Brenden Yule <brendenyule@gmail.com>
2015-08-19 23:37:22 -0500

    Add most system files for i3wm and keybindings

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
-ln -sfv "$dotfiles/editor/.vimrc" ~
-ln -sfv "$dotfiles/editor/.tmux.conf" ~
-ln -sfv "$dotfiles/system/.bashrc" ~
-ln -sfv "$dotfiles/system/.bash_aliases" ~
-ln -sfv "$dotfiles/system/.gitconfig" ~
+ln -sfv "$dotfiles/editor/.tmux.conf" "~"
+ln -sfv "$dotfiles/editor/.vimrc" "~"
+
+ln -sfv "$dotfiles/system/.bash_aliases" "~"
+ln -sfv "$dotfiles/system/.bashrc" "~"
+ln -sfv "$dotfiles/system/.colors" "~"
+ln -sfv "$dotfiles/system/.gitconfig" "~"
+ln -sfv "$dotfiles/system/.Xdefaults" "~"
+
+ln -sfv "$dotfiles/system/xkb/" "/usr/share/X11/"
+ln -sfv "$dotfiles/system/xinitrc" "/etc/X11/xinit/"
+
+ln -sfv "$dotfiles/system/wm/.i3/" "~"
 
 sudo apt-get install vim
 sudo apt-get install tmux
-
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-wait $!
-
-bash < <(curl -sL https://raw.github.com/railsgirls/installation-scripts/master/rails-install-ubuntu.sh)
-wait $!
-rvm install ruby --latest
-rvm reset
 

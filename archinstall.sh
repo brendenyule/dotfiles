e0f53d1ae0407f417cc55bbc8af4425241f0ff93
Brenden Yule <brendenyule@gmail.com>
2015-11-10 17:21:42 -0600

    Small improvemnts to install script

--- archinstall.sh
+++ archinstall.sh
@@ -1,6 +1,23 @@
 #---THINGS YOU WILL NEED---
+# Font Rendering: Infintality
 #
-#
+
+sudo pacman -S vim
+sudo pacman -S neovim
+sudo pacman -S tmux
+sudo pacman -S i3
+sudo pacman -S conky
+sudo pacman -S dunst
+sudo pacman -S mutt
+sudo pacman -S irssi
+sudo pacman -S install udev
+#sudo pacman -S udevil
+sudo pacman -S rxvt-unicode-patched
+sudo pacman -S compton
+sudo pacman -S the_silver_searcher
+sudo pacman -S unclutter
+sudo pacman -S chromium
+
 #https://github.com/radiosilence/irssi-dunst
 ln -sfv "/home/doctorly/dotfiles/editor/.tmux.conf" ~
 ln -sfv "/home/doctorly/dotfiles/editor/.vimrc" ~
@@ -14,21 +31,7 @@ ln -sfv "/home/doctorly/dotfiles/system/.Xdefaults" ~
 #sudo ln -sfv "/home/doctorly/dotfiles/system/xkb/" "/usr/share/X11/"
 sudo ln -sfv "/home/doctorly/dotfiles/system/xinitrc" "/etc/X11/xinit/"
 
-ln -sfv "/home/doctorly/dotfiles/system/wm/.i3/" ~
-
-sudo pacman -S vim
-sudo pacman -S neovim
-sudo pacman -S tmux
-sudo pacman -S i3
-sudo pacman -S conky
-sudo pacman -S dunst
-sudo pacman -S mutt
-sudo pacman -S irssi
-sudo pacman -S install udev
-sudo pacman -S udevil
-sudo pacman -S rxvt-unicode-patched
-sudo pacman -S compton
-sudo pacman -S the_silver_searcher
-sudo pacman -S unclutter
-sudo pacman -S chromium
+ln -sfv "/home/doctorly/dotfiles/system/wm/.i3/config" "~/.i3/config"
+ln -sfv "/home/doctorly/dotfiles/system/wm/.i3/conky" "~/.i3/conky"
+ln -sfv "/home/doctorly/dotfiles/system/wm/.i3/conky.sh" "~/.i3/conky.sh"
 

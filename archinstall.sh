819104b5fa552eeb7df8201ec41eff9b5d5bc82a
Brenden Yule <brendenyule@Brendens-MacBook-Air.local>
2016-12-19 20:42:34 -0600

    basic ubuntu updates

--- archinstall.sh
+++ archinstall.sh
@@ -1,28 +1,64 @@
-#---THINGS YOU WILL NEED---
+#---THINGS YOU WILL NEED-
 # Font Rendering: Infintality
-#
 
-sudo pacman -S vim
-sudo pacman -S neovim
-sudo pacman -S tmux
-sudo pacman -S i3
-sudo pacman -S conky
-sudo pacman -S dunst
-sudo pacman -S mutt
-sudo pacman -S irssi
+#need i3 log directory
+#need copy-paste support for urxvt
+#look into an actual keymapping config
+
 #sudo pacman -S install udev
-sudo pacman -S install udiskie
 #sudo pacman -S udevil
-sudo pacman -S rxvt-unicode-patched
-sudo pacman -S compton
-sudo pacman -S the_silver_searcher
-sudo pacman -S unclutter
+sudo pacman -S base-devel
 sudo pacman -S chromium
+sudo pacman -S compton
+sudo pacman -S conky
+sudo pacman -S curl
+sudo pacman -S dmenu
+sudo pacman -S dunst
 sudo pacman -S git
+sudo pacman -S htop
+sudo pacman -S i3
+sudo pacman -S install udiskie
+sudo pacman -S irssi
+sudo pacman -S libinput
+sudo pacman -S mutt
+sudo pacman -S neovim
+sudo pacman -S the_silver_searcher
+sudo pacman -S tmux
+sudo pacman -S unclutter
+sudo pacman -S vim
+sudo pacman -S xf86-input-libinput
+
+sudo pacman -Rs xf86-input-synaptics
+
+mkdir aur
+cd aur
+git clone https://aur.archlinux.org/package-query.git
+cd package-query
+makepkg -sri
+cd ..
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt
+makepkg -sri
+cd ..
+
+https://aur.archlinux.org/light.git
+cd light
+makepkg -sri
+cd
+
+yaourt -S rxvt-unicode-patched
+yaourt -S ttf-ubuntu-font-family
+yaourt -S ttf-font-awesome
+yaourt -S light
+yaourt -S tintin
+
+
+#neovim plugin manager
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 #https://github.com/radiosilence/irssi-dunst
 ln -sfv "/home/doctorly/dotfiles/editor/.tmux.conf" ~
-ln -sfv "/home/doctorly/dotfiles/editor/.vimrc" ~
 ln -sfv "/home/doctorly/dotfiles/editor/.vimrc" "/home/doctorly/.config/nvim/init.vim"
 
 ln -sfv "/home/doctorly/dotfiles/system/.bash_aliases" ~
@@ -30,13 +66,17 @@ ln -sfv "/home/doctorly/dotfiles/system/.bashrc" ~
 ln -sfv "/home/doctorly/dotfiles/system/.colors" ~
 ln -sfv "/home/doctorly/dotfiles/system/.gitconfig" ~
 ln -sfv "/home/doctorly/dotfiles/system/.Xdefaults" ~
+ln -sfv "/home/doctorly/dotfiles/system/compton.conf" "/home/doctorly/.config/"
 
 #sudo ln -sfv "/home/doctorly/dotfiles/system/xkb/" "/usr/share/X11/"
-sudo ln -sfv "/home/doctorly/dotfiles/system/xinitrc" "~/.xinitrc"
+sudo ln -sfv "/home/doctorly/dotfiles/system/xinitrc" "/home/doctorly/.xinitrc"
 
-ln -sfv "/home/doctorly/dotfiles/system/wm/.i3/config" "~/.i3/config"
-ln -sfv "/home/doctorly/dotfiles/system/wm/.i3/conky" "~/.i3/conky"
-ln -sfv "/home/doctorly/dotfiles/system/wm/.i3/conky.sh" "~/.i3/conky.sh"
+mkdir -p /home/doctorly/logs/i3
+ln -sf "/home/doctorly/dotfiles/wm/.i3/config" "/home/doctorly/.i3/config"
+ln -sf "/home/doctorly/dotfiles/wm/.i3/conky" "/home/doctorly/.i3/conky"
+ln -sf "/home/doctorly/dotfiles/wm/.i3/conky.sh" "/home/doctorly/.i3/conky.sh"
+sudo cp -rf "/home/doctorly/dotfiles/system/xkb/" "/usr/share/X11/"
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+sudo ln -sf "/home/doctorly/dotfiles/system/urxvtclip" "/usr/lib/urxvt/perl/clipboard"
+sudo ln -sf "/home/doctorly/dotfiles/system/urxvtclip" "/usr/lib64/urxvt/perl/clipboard"
 
--- install.sh
+++ install.sh
@@ -1,57 +1,68 @@
+#sudo add-apt-repository ppa:neovim-ppa/unstable
+#sudo add-apt-repository ppa:linrunner/tlp
+#sudo apt-get update
+
+sudo apt-get install software-properties-common
+sudo apt-get install python-software-properties
 sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo add-apt-repository ppa:linrunner/tlp
 sudo apt-get update
 sudo apt-get install python-dev python-pip python3-dev python3-pip
-
-
-sudo apt-get install vim
 sudo apt-get install neovim
-sudo apt-get install tmux
-sudo apt-get install i3
+
+#sudo pacman -S chromium
+#sudo pacman -S google-chrome-stable
+#sudo apt-get install compton
 sudo apt-get install conky
-sudo apt-get install irssi
-sudo apt-get install udevil
-sudo apt-get install rxvt-unicode-256color
-sudo apt-get install wicd-curses
-sudo apt-get install compton
-sudo apt-get install unclutter
+sudo apt-get install curl
+sudo apt-get install dmenu
+#sudo apt-get install dunst
 sudo apt-get install git
-sudo apt-get install xclip
+sudo apt-get install htop
+sudo apt-get install i3
+sudo apt-get install irssi
+sudo apt-get install mutt
+sudo apt-get install neovim
 sudo apt-get install silversearcher-ag
+sudo apt-get install tmux
+sudo apt-get install unclutter
+sudo apt-get install rxvt-unicode-256color
 
-ln -sfv ~/dotfiles/editor/.tmux.conf ~
-ln -sfv ~/dotfiles/editor/.vimrc ~
-ln -sfv ~/dotfiles/editor/.vimrc ~/.config/nvim/init.vim
 
-ln -sfv ~/dotfiles/system/.bash_aliases ~
-ln -sfv ~/dotfiles/system/.bashrc ~
-ln -sfv ~/dotfiles/system/.colors ~
-ln -sfv ~/dotfiles/system/.gitconfig ~
-ln -sfv ~/dotfiles/system/.Xdefaults ~
-
-#sudo ln -sfv "/home/doctorly/dotfiles/system/xkb/" "/usr/share/X11/"
-sudo ln -sfv "~/dotfiles/system/xinitrc" "/etc/X11/xinit/"
-
-mkdir .i3
-ln -sfv ~/dotfiles/wm/.i3/conky ~/.i3/.
-ln -sfv ~/dotfiles/wm/.i3/conky.sh ~/.i3/.
-ln -sfv ~/dotfiles/wm/.i3/config ~/.i3/.
-
-ln -sfv ~/dotfiles/system/xkb/keycodes/evdev /usr/share/X11/xkb/keycodes/evdev
-sudo rm -rf /var/lib/xkb/*
-
-#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-curl -Lo ~/.config/nvim/autoload/plug.vim --create-dirs \
+#neovim plugin manager
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+#https://github.com/radiosilence/irssi-dunst
+ln -sfv "/home/doctorly/dotfiles/editor/.tmux.conf" ~
+ln -sfv "/home/doctorly/dotfiles/editor/.vimrc" "/home/doctorly/.config/nvim/init.vim"
+
+ln -sfv "/home/doctorly/dotfiles/system/.bash_aliases" ~
+ln -sfv "/home/doctorly/dotfiles/system/.bashrc" ~
+ln -sfv "/home/doctorly/dotfiles/system/.colors" ~
+ln -sfv "/home/doctorly/dotfiles/system/.gitconfig" ~
+ln -sfv "/home/doctorly/dotfiles/system/.Xdefaults" ~
+ln -sfv "/home/doctorly/dotfiles/system/compton.conf" "/home/doctorly/.config/"
+
+#sudo ln -sfv "/home/doctorly/dotfiles/system/xkb/" "/usr/share/X11/"
+sudo ln -sfv "/home/doctorly/dotfiles/system/xinitrc" "/home/doctorly/.xinitrc"
+
+mkdir -p /home/doctorly/logs/i3
+ln -sf "/home/doctorly/dotfiles/wm/.i3/config" "/home/doctorly/.i3/config"
+ln -sf "/home/doctorly/dotfiles/wm/.i3/conky" "/home/doctorly/.i3/conky"
+ln -sf "/home/doctorly/dotfiles/wm/.i3/conky.sh" "/home/doctorly/.i3/conky.sh"
+sudo cp -rf "/home/doctorly/dotfiles/system/xkb/" "/usr/share/X11/"
+
+sudo ln -sf "/home/doctorly/dotfiles/system/urxvtclip" "/usr/lib/urxvt/perl/clipboard"
+sudo ln -sf "/home/doctorly/dotfiles/system/urxvtclip" "/usr/lib64/urxvt/perl/clipboard"
+
+
 # OSX features
-sudo apt-get install powertop
-sudo apt-get install tlp tlp-rdw
- from ttps://help.ubuntu.com/community/MacBookAir6-2/Trusty?action=AttachFile&do=view&target=tlp
-sudo mkdir /etc/default/tlp
-mkdir ~/src
-git clone https://github.com/hobarrera/kbdlight/ ~/src/.
-make ~/src/kbdlight
+#sudo apt-get install powertop
+#sudo apt-get install tlp tlp-rdw
+ #from ttps://help.ubuntu.com/community/MacBookAir6-2/Trusty?action=AttachFile&do=view&target=tlp
+#sudo mkdir /etc/default/tlp
+#mkdir ~/src
+#git clone https://github.com/hobarrera/kbdlight/ ~/src/.
+#make ~/src/kbdlight
 #sudo curl -Lo /etc/default/tlp https://help.ubuntu.com/community/MacBookAir6-2/Trusty?action=AttachFile&do=get&target=tlp
 
